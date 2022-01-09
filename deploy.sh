f1c6cfce738e75156a5fd055ba126faafe5ff13f
avasyutina <avasyutina@diasoft.ru>
2022-01-09 10:39:10 +0300

    DZ topic 6

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install git
+sudo apt-get install -y git
 git clone -b monolith https://github.com/express42/reddit.git
-cd reddit && bundle install
+cd ./reddit && bundle install
 puma -d
-#ps aux | grep puma
+ps aux | grep puma
