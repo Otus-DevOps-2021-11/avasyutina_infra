4e60d086f26129d8d218f9b09fdac9bf0813dda3
ot <avasyutina@diasoft.ru>
2022-01-16 10:57:39 +0700

    HW topic 8, terraform-1

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ resource "yandex_compute_instance" "app" {
   }
 
   metadata = {
-    ssh-keys = "ubuntu:${file("var.public_key_path")}"
+    ssh-keys = "ubuntu:${file(var.public_key_path)}"
   }
 
   boot_disk {
