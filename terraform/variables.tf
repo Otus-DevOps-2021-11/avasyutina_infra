2b7f9ab8016c327840aea1e1e25505a8ada7ee64
avasyutina <96159008+avasyutina@users.noreply.github.com>
2022-01-17 08:04:16 +0700

    Terraform 1 (#5)
    * HW topic 8, terraform-1
    
    * HW topic 8, terraform-1
    
    * HW topic 8, terraform-1
    
    * HW topic 8, terraform-1
    
    * HW topic 8, terraform-1
    
    * HW topic 8, terraform-1
    
    * HW topic 8, terraform-1
    
    * HW topic 8, terraform-1
    
    * HW topic 8, terraform-1

---
+++ terraform/variables.tf
@@ -0,0 +1,31 @@
+variable cloud_id {
+  description = "cloud"
+}
+variable folder_id {
+  description = "folder"
+}
+variable zone {
+  description = "Zone"
+  # Значение по умолчанию
+  default = "ru-central1-a"
+}
+variable service_account_key_file {
+  # Описание переменной
+  description = "acc_key"
+}
+variable image_id {
+  # Описание переменной
+  description = "image"
+}
+variable subnet_id {
+  # Описание переменной
+  description = "net"
+}
+variable public_key_path {
+  # Описание переменной
+  description = "key_pub"
+}
+variable private_key {
+  # Описание переменной
+  description = "key"
+}
